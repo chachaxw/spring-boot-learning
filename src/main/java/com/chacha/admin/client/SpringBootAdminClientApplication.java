72567320013bf8b7ead622a6196abd8c24a0f527
chacha <867571123@qq.com>
2020-10-02 15:35:41 +0800

    feat: add SpringBootAdminClientApplication and IndexController

--- src/main/java/com/chacha/admin/client/SpringBootAdminClientApplication.java
+++ src/main/java/com/chacha/admin/client/SpringBootAdminClientApplication.java
@@ -1,2 +1,21 @@
-package com.chacha.admin.client;public class SpringBootAdminClientApplication {
+package com.chacha.admin.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ *     SpringBoot Client 启动类
+ * </p>
+ *
+ * @package: com.chacha.helloworld
+ * @description: SpringBoot Client 启动类
+ * @author: chacha
+ */
+@SpringBootApplication
+public class SpringBootAdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminClientApplication.class, args);
+    }
 }
--- src/main/java/com/chacha/admin/client/controller/IndexController.java
+++ src/main/java/com/chacha/admin/client/controller/IndexController.java
@@ -1,2 +1,21 @@
-package com.chacha.admin.client.controller;public class IndexController {
+package com.chacha.admin.client.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *     Index page
+ * </p>
+ *
+ * @package: com.chacha.helloworld
+ * @description: Index page
+ * @author: chacha
+ */
+@RestController
+public class IndexController {
+    @GetMapping(value = {"", "/"})
+    public String index() {
+        return "This is a spring boot admin client demo";
+    }
 }
