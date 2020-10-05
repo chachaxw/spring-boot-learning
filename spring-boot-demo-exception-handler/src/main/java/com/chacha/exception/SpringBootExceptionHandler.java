1cd36bb713fb2f650d2d0dc141ff9ee966a910e1
chacha <867571123@qq.com>
2020-10-05 20:51:26 +0800

    feat: init spring boot demo exception handler module

---
+++ spring-boot-demo-exception-handler/src/main/java/com/chacha/exception/SpringBootExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.chacha.exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ *      SpringBoot启动类
+ *      Demo: exception handler 接口
+ * </p>
+ *
+ * @package: com.chacha.exception
+ * @description: SpringBoot启动类
+ * @author chacha
+ */
+@SpringBootApplication
+public class SpringBootExceptionHandler {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExceptionHandler.class, args);
+    }
+}
