a3678a0e2832bf3737068fe63cd7634120c8fd2f
chacha <867571123@qq.com>
2020-10-06 11:19:48 +0800

    feat: add BaseException class

--- spring-boot-demo-exception-handler/src/main/java/com/chacha/exception/exception/BaseException.java
+++ spring-boot-demo-exception-handler/src/main/java/com/chacha/exception/exception/BaseException.java
@@ -1,2 +1,33 @@
-package com.chacha.exception.exception;public class BaseException {
+package com.chacha.exception.exception;
+
+import com.chacha.exception.constant.Status;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * <p>
+ *      异常基类
+ * </p>
+ *
+ * @package: com.chacha.handler.exception
+ * @description: 异常基类
+ * @author chacha
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class BaseException extends RuntimeException {
+    private Integer code;
+    private String message;
+
+    public BaseException(Status status) {
+        super(status.getMessage());
+        this.code = status.getCode();
+        this.message = status.getMessage();
+    }
+
+    public BaseException(Integer code, String message) {
+        super(message);
+        this.code = code;
+        this.message = message;
+    }
 }
