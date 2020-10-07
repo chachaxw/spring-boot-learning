1b0cdc3123cf9f20f8e96bad98ce5c094f5fc5ad
chacha <867571123@qq.com>
2020-10-07 21:39:37 +0800

    feat: add JsonException class

--- spring-boot-demo-exception-handler/src/main/java/com/chacha/exception/exception/JsonException.java
+++ spring-boot-demo-exception-handler/src/main/java/com/chacha/exception/exception/JsonException.java
@@ -1,2 +1,25 @@
-package com.chacha.exception.exception;public class JsonException {
+package com.chacha.exception.exception;
+
+import com.chacha.exception.constant.Status;
+import lombok.Getter;
+
+/**
+ * <p>
+ *      JSON异常
+ * </p>
+ *
+ * @package: com.chacha.handler.exception
+ * @description: JSON异常
+ * @author chacha
+ */
+@Getter
+public class JsonException extends BaseException {
+
+    public JsonException(Status status) {
+        super(status);
+    }
+
+    public JsonException(Integer code, String message) {
+        super(code, message);
+    }
 }
