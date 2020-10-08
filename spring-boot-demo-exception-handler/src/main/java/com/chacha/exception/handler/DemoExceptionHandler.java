a5a551b8f9922b2b1cbb6b47c748382d1d4f3186
chacha <867571123@qq.com>
2020-10-08 11:40:26 +0800

    feat: add DemoExceptionHandler class

---
+++ spring-boot-demo-exception-handler/src/main/java/com/chacha/exception/handler/DemoExceptionHandler.java
@@ -0,0 +1,53 @@
+package com.chacha.exception.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import com.chacha.exception.exception.JsonException;
+import com.chacha.exception.exception.PageException;
+import com.chacha.exception.model.ApiResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * <p>
+ *     统一异常处理
+ * </p>
+ *
+ * @package: com.chacha.exception
+ * @description: 统一异常处理
+ * @author chacha
+ */
+@Slf4j
+@ControllerAdvice
+public class DemoExceptionHandler {
+    private static final String DEFAULT_ERROR_VIEW = "error";
+
+    /**
+     * 统一 json 异常处理
+     *
+     * @param exception JsonException
+     * @return 统一返回 json 格式
+     */
+    @ExceptionHandler(value = JsonException.class)
+    @ResponseBody
+    public ApiResponse jsonErrorHandler(JsonException exception) {
+        log.error("【JsonException】:{}", exception.getMessage());
+        return ApiResponse.ofException(exception);
+    }
+
+    /**
+     * 统一 页面 异常处理
+     *
+     * @param exception PageException
+     * @return 统一跳转到异常页面
+     */
+    @ExceptionHandler(value = PageException.class)
+    public ModelAndView pageErrorHandler(PageException exception) {
+        log.error("【DemoPageException】:{}", exception.getMessage());
+        ModelAndView view = new ModelAndView();
+        view.addObject("message", exception.getMessage());
+        view.setViewName(DEFAULT_ERROR_VIEW);
+        return view;
+    }
+}
